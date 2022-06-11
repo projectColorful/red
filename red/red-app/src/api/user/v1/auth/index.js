e29270f4efe89b15609e21b0b1948f58c03516a8
jungkihunm <khkh0130@gmail.com>
2022-06-12 03:38:38 +0900

    jkh file add getname

--- red/red-app/src/api/user/v1/auth/index.js
+++ red/red-app/src/api/user/v1/auth/index.js
@@ -10,6 +10,6 @@ module.exports = (app) => {
     app.group([passport.authenticate('user.jwt', { session: false })], (router) => {
         router.post('/join ', require('./join'));
         router.post('/pwChage', require('./pwChage'));
-        router.get('/getname', require('./pwChage'));
+        router.get('/getname', require('./getName'));
     });
 };
