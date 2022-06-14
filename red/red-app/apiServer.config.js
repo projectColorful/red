207de1a8b447b71668bc680762da512162887f63
jungkihunm <khkh0130@gmail.com>
2022-06-14 23:45:50 +0900

    jkh file bug fix

---
+++ red/red-app/apiServer.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps:[{
+        name:'app',
+        script:'./app.js',
+        instances: 2,//코어 수 0은 제한 없음 
+        exec_mode:'cluster',
+        merge_logs:true,
+        autorestart:true,
+        watch:process.env.NODE_ENV
+    }]
+}
