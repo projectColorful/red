f7841a684e3d5e96d2cdf90b5a0965004297aab8
jungkihunm <khkh0130@gmail.com>
2022-06-14 23:46:55 +0900

    jkh file bug fix

--- red/red-app/app.js
+++ red/red-app/app.js
@@ -3,11 +3,9 @@ const path = require('path');
 const morgan = require("morgan");
 const passport = require('passport');
 const cors = require('cors');
-//const compression = require('compression');
 const jkh_f = require('./lib/jkh_function');
 require('./lib/express_group');
 const app = express();
-//app.use(compression())
 app.disable('x-powered-by'); // x-powered-by 헤더 비활성화
 app.use(cors({
 	exposedHeaders: ['Content-Disposition'], // 다운로드 시 파일명 첨부 허용
@@ -15,7 +13,6 @@ app.use(cors({
 app.options('*', cors()); // CORS Pre-Flight 활성화
 app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
-// app.use(cookieParser());
 app.use(passport.initialize());//passport 실행
 app.get('/',(req, res) => {
     return res.send("Hello world");
@@ -23,7 +20,7 @@ app.get('/',(req, res) => {
 app.use(morgan('combined', { stream: jkh_f.logstream }))//로그파일로 관리 함 1일단위
 
 app.use('/api/v1/user', require('./src/api/user/v1/')); //사용자
-// app.use('/api/v1/admin', admin); //관리자
+// app.use('/api/v1/admin', admin); //admin page
 app.listen(3000, () => {
     console.log('http://localhost:3000');
 });
