b4f05a9afa022999f3fd5630e125c2cd9cfaaffe
jungkihunm <khkh0130@gmail.com>
2022-06-18 23:37:02 +0900

    jkh fix

--- red/red-app/src/api/user/v1/auth/index.js
+++ red/red-app/src/api/user/v1/auth/index.js
@@ -5,7 +5,7 @@ const passport = require(`../passport`);
  * @description /login -> 로그인
  * @description /pwChage -> 비밀번호 변경
  */
- const test = async (req, res) => {
+const test = async (req, res) => {
     var ress = [];
     for (let i = 0; i < 100; i++) {
         ress.push({
@@ -21,10 +21,13 @@ const passport = require(`../passport`);
     return res.status(200).json(ress);
 }/// 테스트 함수
 module.exports = (app) => {
-    app.post('/login', [passport.authenticate('user.local', { session: false })], require('./login'));
-    app.get('/join ', require('./join') );
-    app.post('/pwchage', require('./pwChage'));
-    app.get('/test',test);
+    app.group((router) => {
+        router.post('/login', [passport.authenticate('user.local', { session: false })], require('./login'));
+        router.get('/join ', require('./join'));
+        router.post('/pwchage', require('./pwChage'));
+        router.get('/test', test);
+    })
+
     app.group([passport.authenticate('user.jwt', { session: false })], (router) => {
         router.get('/getname', require('./getName'));
     });
