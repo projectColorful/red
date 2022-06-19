d9ba156e58fe4667a5208e01c6fc72a2b787fe13
jungkihunm <khkh0130@gmail.com>
2022-06-20 01:03:26 +0900

    jkh dockerfile add

--- red/red-app/apiServer.config.js
+++ red/red-app/apiServer.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     apps:[{
-        name:'app',
+        name:'red',
         script:'./app.js',
         instances: 2,//코어 수 0은 제한 없음 
         exec_mode:'cluster',
         merge_logs:true,
         autorestart:true,
-        watch:process.env.NODE_ENV
+        watch:process.env.NODE_ENV === 'development'
     }]
 }
