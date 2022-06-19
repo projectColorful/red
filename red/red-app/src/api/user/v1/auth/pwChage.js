4ff942b6e1f6d008b2d05425beeb558b224c487a
jungkihunm <khkh0130@gmail.com>
2022-06-20 00:25:42 +0900

    jkh pwchage fix end

--- red/red-app/src/api/user/v1/auth/pwChage.js
+++ red/red-app/src/api/user/v1/auth/pwChage.js
@@ -25,7 +25,8 @@ async function pwChage(req, res) {
             response.msg = 'params is empty !!';
             return res.status(500).json(response);
         }
-        const sql1 = Q`UPDATE users SET user_pw = ${bcrypt.hash(params.user_pw, saltRounds)} WHERE user_id = ${params.user_id} RETURNING *`; //등록
+        console.log(bcrypt.hash(params.user_pw, saltRounds));
+        const sql1 = Q`UPDATE users SET user_pw = ${await bcrypt.hash(params.user_pw, saltRounds)} WHERE user_id = ${params.user_id} RETURNING *`; //등록
         const query1 = await pool.query(sql1); //값 저장
 
         if (jkh.isEmpty(query1.rows)) {
