9270a3f96e9e5241595f6f3153fab0387cbea1a7
jungkihunm <khkh0130@gmail.com>
2022-06-21 12:28:40 +0900

    jkh winston add file

--- red/red-app/app.js
+++ red/red-app/app.js
@@ -8,9 +8,7 @@ const jkh_f = require('./lib/jkh_function');
 require('./lib/express_group');
 const app = express();
 app.disable('x-powered-by'); // x-powered-by 헤더 비활성화
-app.use(cors({
-    exposedHeaders: ['Content-Disposition'], // 다운로드 시 파일명 첨부 허용
-})); // CORS 해제
+// CORS 해제
 app.options('*', cors()); // CORS Pre-Flight 활성화
 app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
